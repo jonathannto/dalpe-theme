d9eda53ab6fd671078375c4615ed9222e1f092b2
Jonathan Nascimento <dev@jonathan.eng.br>
2026-01-03 20:56:24 -0300

    loop em index

--- index.php
+++ index.php
@@ -1,27 +1,49 @@
-<?php get_header(); ?>
-        <div class="content-area">
-            <main>
-                <section class="slider">Slider</section>
-                <section class="popular-products">
-                    <div class="section-title">
-                        <h2>Popular Products</h2>
-                    </div>
-                </section>
-                <section class="new-arrivals">
-                    <div class="section-title">
-                        <h2>New Arrivals</h2>
-                    </div>
-                </section>
-                <section class="deal-of-the-week">
-                    <div class="section-title">
-                        <h2>Deal of the Week</h2>
-                    </div>
-                </section>
-                <section class="site-blog">
-                    <div class="section-title">
-                        <h2>Latest News</h2>
-                    </div>
-                </section>
-            </main>
-        </div>
-<?php get_footer(); ?>      
+<?php get_header(); ?> <div class="content-area">
+    <main>
+        <section class="slider">Slider</section>
+        <section class="popular-products">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Popular Products</h2>
+                </div>
+            </div>
+        </section>
+        <section class="new-arrivals">
+            <div class="container">
+                <div class="section-title">
+                    <h2>New Arrivals</h2>
+                </div>
+            </div>
+        </section>
+        <section class="deal-of-the-week">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Deal of the Week</h2>
+                </div>
+                <div class="row"> </div>
+            </div>
+        </section>
+        <section class="site-blog">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Latest News</h2>
+                </div>
+                <div class="row">
+                    <?php
+                    if (have_posts()):
+                        while (have_posts()): the_post(); ?>
+                            <article>
+                                <h1><?php the_title(); ?></h1>
+                                <div class="content"><?php the_content(); ?></div>
+                            </article> 
+                            <?php
+                                endwhile;
+                                else:
+                            ?>
+                        ​<p>Nothing to display</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </section>
+    </main>
+</div> <?php get_footer(); ?>
